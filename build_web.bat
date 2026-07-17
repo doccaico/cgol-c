3297790e83552a32d988aab5021679b118889cf1
ociaccod <hash_ext_luna@yahoo.co.jp>
2026-07-17 17:10:45 +0900

    Up

--- build_web.bat
+++ build_web.bat
@@ -8,12 +8,12 @@ set FILES="vendor\raylib\bin\wasm\libraylib.web.a"
 
 set "FLAGS=-DPLATFORM_WEB"
 set "FLAGS=%FLAGS% -I ./vendor/raylib/include"
-set "FLAGS=%FLAGS% -sEXPORTED_RUNTIME_METHODS=['HEAPF32'] -sUSE_GLFW=3 -sWASM_BIGINT -sWARN_ON_UNDEFINED_SYMBOLS=0 -sASSERTIONS --shell-file src\index_template.html"
+set "FLAGS=%FLAGS% -sEXPORTED_RUNTIME_METHODS=['HEAPF32'] -sUSE_GLFW=3 -sWASM_BIGINT -sWARN_ON_UNDEFINED_SYMBOLS=0  --shell-file src\index_template.html"
 
 if "%~1" == "--debug" (
-    REM put something
+    set "FLAGS=%FLAGS% -sASSERTIONS=1 -sSTACK_OVERFLOW_CHECK=1"
 ) else if "%~1" == "--release" (
-    set "FLAGS=%FLAGS% -Os"
+    set "FLAGS=%FLAGS% -Os -flto"
 )
 
 cmd /c emcc -o %OUT_DIR%\index.html src\main.c %FLAGS% %FILES% 
